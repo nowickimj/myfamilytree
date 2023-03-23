402ac5939d023ada49202354b50c47edd8ec9dfa
marcin <nowicki.marcin.j@gmail.com>
2023-03-23 19:00:40 +0100

    Initial commit

---
+++ src/main/java/net/mnowicki/familia/FamiliaApplication.java
@@ -0,0 +1,13 @@
+package net.mnowicki.familia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamiliaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamiliaApplication.class, args);
+	}
+
+}
