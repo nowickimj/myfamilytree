c8bed59272439a9638c979cac8428189c3288b8f
marcin <nowicki.marcin.j@gmail.com>
2023-05-01 20:44:44 +0200

    Fix for mongo init

--- docker/mongodb/init/mongo-init.js
+++ docker/mongodb/init/mongo-init.js
@@ -18,12 +18,12 @@ db.configuration.insertOne({
 });
 
 db.createCollection("user");
-db.configuration.insertOne({
+db.user.insertOne({
     email: "admin",
     password: "$2a$12$J4jFaOw.gTf5BzVfbJn1JuG9S09jni5R/l47GOj0TpakEmUCDyuoW",
     roles: ["USER, ADMIN"]
 });
-db.configuration.insertOne({
+db.user.insertOne({
     email: "test",
     password: "$2a$12$tiZHFlu/Zc.r2JiuclD9ceLc05JrapEnHdJidt5D1OMzEJJzY5evO",
     roles: ["USER"]
